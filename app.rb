ba311033237f8995d07a97c839634bc0ce7e7376
madelinemc <madeline.coons@gmail.com>
2020-11-09 14:27:54 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
+	end # this tells sinatra to render a file called index.erb inside of the director called views
+
+	get "/info" do
+		erb :info
 	end
+
 end
